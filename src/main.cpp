25d5d657fc15e41a63b260058ee7243ae32d683b
Nefta007 <nperc001@ucr.edu>
2025-09-04 23:56:49 -0700

    created main for dragon radar

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+
+/* 
+code will scan for decvice nearby 
+if ball is detected then we will print it in serial 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);       //Starts serial monitor
+  BLEDevice::init("");        //Initializes BLE --- not scanner does not need name
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setActiveScan(true);        //Actice scan function is to get details such as device name
+
+  //will scan for 2 seconds
+  BLEScanResults results = pBLEScan->start(2);
+
+  //we want to loop through all devices foudn
+  for(int i = 0; i < results.getCount(); i++){
+    BLEAdvertisedDevice device = results.getDevice(i);
+
+    //get device name and strength from signal
+    String name = device.getName().c_str();
+    int rssi = device.getRSSI();
+
+    //Check if it is desired device
+    if(name.startsWith("Dragon_Ball")){
+      Serial.print("Found: ");
+      Serial.print(name);       //will print name of device
+      Serial.print(" | Strength of Signal (RSSI) is ");
+      Serial.print(rssi);       //will print signal strength --- note!!!! higher value (-40 or closer) indicates close --> Lower (-80) indicates farther
+    }
+  }
+
+  //wait 1 seconds before scanning again
+  delay(1000);
+}
